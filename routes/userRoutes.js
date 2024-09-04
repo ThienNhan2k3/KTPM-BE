6a007be3a0c4cf0d74fac2c113639861f6c8927d
lpchi133 <lpchi21@clc.fitus.edu.vn>
2024-09-04 16:05:16 +0700

    push code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,9 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/getAll', userController.getAll);
-//router.post('/create', userController.create);
-//router.get('/get/:uuid', userController.getByUUID);
-//router.put('/update/:uuid', userController.update);
-//router.delete('/delete/:uuid', userController.delete);
+router.post('/create', userController.createuser);
+router.get('/get/:uuid', userController.getuserByUUID);
+router.put('/update/:uuid', userController.updateuser);
+router.delete('/delete/:uuid', userController.deleteuser);
 
 module.exports = router;
