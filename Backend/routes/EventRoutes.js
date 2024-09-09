8c12a369402d54cf3d1b0937ef19705c4a5a3aab
PG47 <86549123+PG47@users.noreply.github.com>
2024-09-09 13:12:03 +0700

    Update upload Event image

--- Backend/routes/EventRoutes.js
+++ Backend/routes/EventRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const EventController = require('../controllers/EventController.js');
-const upload = require("../middlewares/uploadFile.js");
+const { upload } = require("../middlewares/uploadFile");
 
 const router = express.Router();
 
 router.get('/getAll', EventController.getAll);
-router.get('/getOngoingEvents', EventController.getOngoingEvents);
-router.post('/create', EventController.create);
+router.post('/create', 
+    upload.single("my_image"),
+    EventController.create);
 //router.get('/get/:uuid', quizEventController.getByUUID);
 router.put('/update/:uuid', EventController.update);
 //router.delete('/delete/:uuid', quizEventController.delete);
@@ -14,15 +15,13 @@ router.get('/getEvent/:id', EventController.getByID);
 
 //favEvent
 router.get('/getAllFavEvent', EventController.getAllFavorite);
+router.get('/checkFavEvent', EventController.checkFavorite);
 router.post('/addFavEvent', EventController.addFavorite)
 router.delete('/deleteFavEvent', EventController.deleteFavorite)
 
-//Lac xi
-router.get("/lacxi/:uuid/:userId", EventController.playLacXiEvent); 
+router.get("/lacxi/:uuid", EventController.playLacXiEvent); 
 router.get("/lacxi/:uuid/redeem", EventController.redeemGift); 
 router.get("/share/:uuid", EventController.shareFb); 
 // router.post("/donate/:uuid", EventController.donateTicket); 
-router.get('/lacxi/getOrCreatePlaythrough/:userId/:eventId', EventController.getOrCreatePlaythrough)
-router.get('/lacxi/getUserItemsForEvent/:userId/:eventId', EventController.getUserItemsForEvent)
 
 module.exports = router;
